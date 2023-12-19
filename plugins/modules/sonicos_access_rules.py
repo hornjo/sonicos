94e3877af3e037540a2a61f99ad8e71588f88f0b
hornjo <Johannes-Horn@hotmail.com>
2023-12-19 09:08:31 +0100

    Added ip version cases

--- plugins/modules/sonicos_access_rules.py
+++ plugins/modules/sonicos_access_rules.py
@@ -11,6 +11,7 @@ from ansible_collections.hornjo.sonicos.plugins.module_utils.sonicos_core_functi
     authentication,
     commit,
     execute_api,
+    compare_json,
 )
 
 __metaclass__ = type
@@ -143,6 +144,17 @@ module_args = dict(
     ),
     comment=dict(type="str", required=False),
     max_connections=dict(type="int", required=False, default=100),
+    logging=dict(type="bool", default=True),
+    sip=dict(type="bool", default=False),
+    h323=dict(type="bool", default=False),
+    management_traffic=dict(type="bool", default=False),
+    packet_monitoring=dict(type="bool", default=False),
+    tcp_urgent_packages=dict(type="bool", default=False),
+    fragment_packages=dict(type="bool", default=True),
+    dpi=dict(type="bool", default=True),
+    dpi_ssl_client=dict(type="bool", default=True),
+    dpi_ssl_server=dict(type="bool", default=True),
+    flow_reporting=dict(type="bool", default=False),
     state=dict(type="str", choices=["present", "absent"], default="present"),
 )
 
@@ -165,50 +177,10 @@ auth_params = (module.params["username"], module.params["password"])
 
 
 # Defining actual module functions
-def get_json_params():
-    """Function builds json parameters"""
-    json_params = {"access_rules": []}
-
-    json_helper = {
-        "ipv4": {
-            "from": module.params["source_zone"],
-            "to": module.params["destination_zone"],
-            "action": module.params["action"],
-            "schedule": {"always_on": True},
-            "name": module.params["rule_name"],
-            "comment": module.params["comment"],
-            "enable": module.params["enable"],
-            "auto_rule": module.params["auto_rule"],
-            "max_connections": module.params["max_connections"],
-            "logging": True,
-            "sip": True,
-            "h323": True,
-            "management": True,
-            "packet_monitoring": True,
-            "packet_dissection_filter": {},
-            "priority": {},
-            "tcp": {"urgent": True, "timeout": 0},
-            "udp": {"timeout": 0},
-            "fragments": True,
-            "botnet_filter": True,
-            "connection_limit": {
-                "destination": {"threshold": {"value": 0}},
-                "source": {"threshold": {"value": 0}},
-            },
-            "dpi": True,
-            "dpi_ssl": {"client": True, "server": True},
-            "flow_reporting": True,
-            "geo_ip_filter": {"enable": True, "global": True, "custom": True},
-            "block": {"countries": {"unknown": True}, "country": [{"name": "string"}]},
-            "quality_of_service": {"class_of_service": {}, "dscp": {}},
-            "cos_override": True,
-            "bandwidth_management": {"egress": {}, "ingress": {}, "usage_tracking": True},
-            "ticket": {"tag1": "string", "tag2": "string", "tag3": "string"},
-            "single_sign_on": True,
-            "redirect_unauthenticated_users_to_log_in": True,
-            "block_traffic_for_single_sign_on": True,
-        }
-    }
+def get_source_destination():
+    """Getting ip version and address type for access rule"""
+    api_endpoint_ip = "ipv4"
+    source_destination_json = {}
 
     source_address_params = {
         "source": {
@@ -216,13 +188,12 @@ def get_json_params():
         }
     }
     if module.params["source_address"] != "any":
+        address_type, ip_version = get_address_type(module.params["source_address"])
+        if ip_version == "ipv6":
+            api_endpoint_ip = ip_version
         source_address_params = {
             "source": {
-                "address": {
-                    get_address_type(module.params["source_address"]): module.params[
-                        "source_address"
-                    ]
-                },
+                "address": {address_type: module.params["source_address"]},
             },
         }
 
@@ -237,7 +208,6 @@ def get_json_params():
         }
 
     source_address_params["source"].update(source_service_params)
-    json_helper["ipv4"].update(source_address_params)
 
     destination_params = {
         "destination": {
@@ -245,17 +215,14 @@ def get_json_params():
         }
     }
     if module.params["destination_address"] != "any":
+        address_type, ip_version = get_address_type(module.params["destination_address"])
+        if ip_version == "ipv6":
+            api_endpoint_ip = ip_version
         destination_params = {
             "destination": {
-                "address": {
-                    get_address_type(module.params["destination_address"]): module.params[
-                        "destination_address"
-                    ]
-                },
+                "address": {address_type: module.params["destination_address"]},
             },
         }
-
-    json_helper["ipv4"].update(destination_params)
 
     service_params = {"service": {"any": True}}
     if module.params["destination_service"] != "any":
@@ -267,7 +234,69 @@ def get_json_params():
             },
         }
 
-    json_helper["ipv4"].update(service_params)
+    source_destination_json.update(service_params)
+    source_destination_json.update(destination_params)
+    source_destination_json.update(source_address_params)
+
+    return source_destination_json, api_endpoint_ip
+
+
+def get_json_params():
+    """Function builds json parameters"""
+    json_params = {"access_rules": []}
+    source_destination_json, api_endpoint_ip = get_source_destination()
+
+    json_helper = {
+        api_endpoint_ip: {
+            "from": module.params["source_zone"],
+            "to": module.params["destination_zone"],
+            "action": module.params["action"],
+            "schedule": {"always_on": True},
+            "comment": "",
+            "enable": module.params["enable"],
+            "auto_rule": module.params["auto_rule"],
+            "max_connections": module.params["max_connections"],
+            "logging": module.params["logging"],
+            "sip": module.params["sip"],
+            "h323": module.params["h323"],
+            "management": module.params["management_traffic"],
+            "packet_monitoring": module.params["packet_monitoring"],
+            "priority": {"auto": True},
+            "tcp": {"urgent": module.params["tcp_urgent_packages"], "timeout": 15},
+            "udp": {"timeout": 30},
+            "fragments": module.params["fragment_packages"],
+            "botnet_filter": False,
+            "connection_limit": {
+                "destination": {},
+                "source": {},
+            },
+            "dpi": module.params["dpi"],
+            "dpi_ssl": {
+                "client": module.params["dpi_ssl_client"],
+                "server": module.params["dpi_ssl_server"],
+            },
+            "flow_reporting": module.params["flow_reporting"],
+            "geo_ip_filter": {"enable": False, "global": True},
+            "block": {"countries": {"unknown": True}},
+            "quality_of_service": {"class_of_service": {}, "dscp": {"preserve": True}},
+            "bandwidth_management": {"egress": {}, "ingress": {}},
+            "redirect_unauthenticated_users_to_log_in": True,
+        }
+    }
+
+    json_helper[api_endpoint_ip].update(source_destination_json)
+
+    name_params = {
+        "name": module.params["rule_name"],
+    }
+    if module.params["rule_name"] is not None:
+        json_helper[api_endpoint_ip].update(name_params)
+
+    comment_params = {
+        "comment": module.params["comment"],
+    }
+    if module.params["comment"] is not None:
+        json_helper[api_endpoint_ip].update(comment_params)
 
     user_params = {
         "users": {"included": {"all": True}, "excluded": {"none": True}},
@@ -280,15 +309,16 @@ def get_json_params():
         exclude_user_params = {"excluded": {"group": module.params["users_exclude"]}}
         user_params["users"].update(exclude_user_params)
 
-    json_helper["ipv4"].update(user_params)
+    json_helper[api_endpoint_ip].update(user_params)
     json_params["access_rules"].append(json_helper)
 
-    return json_params
+    return json_params, api_endpoint_ip
 
 
 def get_address_type(address_name):
     """Determinig the type for source and destination address"""
     address_type = "name"
+    api_ip_endpoint = "ipv4"
 
     for ip_version in "ipv4", "ipv6":
         url = url_base + "address-groups/" + ip_version
@@ -298,9 +328,10 @@ def get_address_type(address_name):
             for item in req.json()["address_groups"]:
                 if item[ip_version]["name"] == address_name:
                     address_type = "group"
-                    return address_type
+                    api_ip_endpoint = ip_version
+                    return address_type, api_ip_endpoint
 
-    return address_type
+    return address_type, api_ip_endpoint
 
 
 def get_service_type(service_name):
@@ -321,8 +352,8 @@ def get_service_type(service_name):
 def access_rules():
     """Creates idempotency of the module and defines action for the api"""
     api_action = None
-    url = url_base + "access-rules/ipv4"
-    json_params = get_json_params()
+    json_params, api_endpoint_ip = get_json_params()
+    url = url_base + "access-rules/" + api_endpoint_ip
 
     if module.params["state"] == "present":
         api_action = "post"
@@ -331,34 +362,50 @@ def access_rules():
 
     # Debug
     # module.fail_json(msg=json_params, **result)
-    module.fail_json(msg=req.json(), **result)
+    # module.fail_json(msg=req.json(), **result)
 
-    if "interfaces" in req.json():
-        for item in req.json()["interfaces"]:
-            if item["ipv4"]["name"] != module.params["interface_name"]:
+    if "access_rules" in req.json():
+        for item in req.json()["access_rules"]:
+            if (
+                item[api_endpoint_ip]["source"]
+                != json_params["access_rules"][0][api_endpoint_ip]["source"]
+            ):
+                continue
+
+            if (
+                item[api_endpoint_ip]["destination"]
+                != json_params["access_rules"][0][api_endpoint_ip]["destination"]
+            ):
+                continue
+
+            if (
+                item[api_endpoint_ip]["service"]
+                != json_params["access_rules"][0][api_endpoint_ip]["service"]
+            ):
+                continue
+
+            if item[api_endpoint_ip]["to"] != json_params["access_rules"][0][api_endpoint_ip]["to"]:
                 continue
 
             if module.params["state"] == "present":
-                api_action = "patch"
+                api_action = "put"
+                api_endpoint = item[api_endpoint_ip]["uuid"]
 
-            keys = [
-                "uuid",
-                "one_arm_mode",
-                "one_arm_peer",
-                "routed_mode",
-            ]
+            keys = ["uuid"]
+            if module.params["rule_name"] is None:
+                keys.append("name")
 
             for key in keys:
                 try:
-                    del item["ipv4"][key]
+                    del item[api_endpoint_ip][key]
                 except KeyError:
                     continue
 
             # Debug
-            # module.fail_json(msg=json_params["interfaces"][0], **result)
+            # module.fail_json(msg=json_params["access_rules"][0], **result)
             # module.fail_json(msg=item, **result)
 
-            if compare_json(item, json_params["interfaces"][0]) is True:
+            if compare_json(item, json_params["access_rules"][0]) is True:
                 if module.params["state"] == "absent":
                     api_action = "delete"
                     break
@@ -366,6 +413,9 @@ def access_rules():
 
     # Debug
     # module.fail_json(msg=api_action, **result)
+
+    if api_action == "put":
+        url = url_base + "access-rules/ipv4" + "/uuid/" + api_endpoint
 
     if api_action is not None:
         execute_api(url, json_params, api_action, auth_params, module, result)
